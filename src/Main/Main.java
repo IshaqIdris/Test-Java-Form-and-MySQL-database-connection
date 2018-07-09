3195c164c563f13639c927f3b6f647d90c99383b
Ishaq Idris <idrismoha2@ecs.vuw.ac.nz>
2018-07-10 08:54:24 +1200

    added project files

---
+++ src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+import GUI.RegistrationPage;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        //creating object of RegistrationForm class
+        new RegistrationPage();
+    }
+}
